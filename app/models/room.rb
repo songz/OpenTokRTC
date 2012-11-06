e67b60cf3c0f2fc9fe7b2a49b7782f33455bde99
Ankur Oberoi <aoberoi@gmail.com>
2012-11-05 20:37:35 -0500

    adding token to Room model. the token is used to generate an obscure channel name on Pusher.

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,22 @@ class Room < ActiveRecord::Base
   attr_accessible :client_id, :description, :feature, :session_id, :title
 
   has_many :clients
+
+	before_create :create_token
+
+	def self.find_by_token(token)
+		self.includes(:clients).where(:token => token).limit(1).first
+	end
+
+	def create_token
+		self.token = strip_for_channel_name(ActiveSupport::SecureRandom.base64(8))
+	end
+
+	def channel_name
+		@channel_name ||= "list-#{Rails.env}-#{strip_for_channel_name(self.token)}"
+	end
+
+	def strip_for_channel_name(str)
+		str.gsub("/", "").gsub("+", "").gsub(/=+$/, "")
+	end
 end
