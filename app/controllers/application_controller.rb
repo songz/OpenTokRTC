58dc866ebed3fe4be84c962290a8d49d6d4983dc
Song Zheng <coldangelfire311@gmail.com>
2012-11-06 16:26:54 -0800

    clean up dom so view is reuseable when user rejoins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def printa a
-    p "=============="
-    p "=============="
-    p "=============="
-    p "=============="
-    p a
-    p "=============="
-    p "=============="
-    p "=============="
-    p "=============="
-  end
 end
