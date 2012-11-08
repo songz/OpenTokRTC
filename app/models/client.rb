10fb557ced814f51d07d67277b7dd2bb935bb62f
Song Zheng <coldangelfire311@gmail.com>
2012-11-07 16:07:12 -0800

    resize images

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,13 +1,15 @@
 class Client < ActiveRecord::Base
   attr_accessible :flag, :imgdata, :point, :room_id, :name
-
   belongs_to :room
-
   before_save :uploadImage
 
   def uploadImage
+    img = MiniMagick::Image.read(Base64.decode64(self.imgdata))
+    img.resize "100x100"
+    img.format "png"
+
     filePath = "/clientImages/#{Time.now.to_f.to_s}.png"
-    AWS::S3::S3Object.store( filePath, Base64.decode64(self.imgdata) , S3Bucket, :access => :public_read )
+    AWS::S3::S3Object.store( filePath, img.to_blob , S3Bucket, :access => :public_read )
     self.imgdata = S3URL+filePath
   end
 end
