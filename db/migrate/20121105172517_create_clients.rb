a35642c4b12130f879b3394cc064c937d290f355
Song Zheng <coldangelfire311@gmail.com>
2012-11-19 11:14:01 -0800

    change migration imgdata to text

--- db/migrate/20121105172517_create_clients.rb
+++ db/migrate/20121105172517_create_clients.rb
@@ -3,7 +3,7 @@ class CreateClients < ActiveRecord::Migration
     create_table :clients do |t|
       t.string :name
       t.integer :room_id
-      t.string :imgdata
+      t.text :imgdata
       t.integer :flag
       t.integer :point
 
