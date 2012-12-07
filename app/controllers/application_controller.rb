e911259969808227c572a136b2f2a734ad763dc6
Song Zheng <coldangelfire311@gmail.com>
2012-12-06 17:32:35 -0800

    https protocol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def redirect_https        
-    #redirect_to :protocol => "https://" unless request.ssl?
+    redirect_to :protocol => "https://" unless request.ssl?
     return true
   end
   before_filter :redirect_https
